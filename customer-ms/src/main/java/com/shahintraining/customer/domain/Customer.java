1923d96674369001c421374ff48ec5374cc8e7d4
Shahinkhalajestani <vatharishok@gmail.com>
2022-02-23 19:59:35 +0330

    added customer datasource and related repository.

--- customer-ms/src/main/java/com/shahintraining/customer/domain/Customer.java
+++ customer-ms/src/main/java/com/shahintraining/customer/domain/Customer.java
@@ -1,17 +1,45 @@
 package com.shahintraining.customer.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import lombok.experimental.Accessors;
 
-@Data
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@Accessors(chain = true,fluent = true)
 public class Customer {
+    @SequenceGenerator(name = "customer-id-sequence" ,sequenceName = "customer-id-sequence")
+    @GeneratedValue(generator = "customer-id-sequence",strategy = GenerationType.SEQUENCE)
+    @Id
     private Long id;
     private String firstName;
     private String lastName;
     private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Customer customer = (Customer) o;
+
+        if (!id.equals(customer.id)) return false;
+        if (!Objects.equals(firstName, customer.firstName)) return false;
+        if (!Objects.equals(lastName, customer.lastName)) return false;
+        return Objects.equals(email, customer.email);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
 }
--- customer-ms/src/main/java/com/shahintraining/customer/service/CustomerService.java
+++ customer-ms/src/main/java/com/shahintraining/customer/service/CustomerService.java
@@ -2,17 +2,19 @@ package com.shahintraining.customer.service;
 
 import com.shahintraining.customer.domain.Customer;
 import com.shahintraining.customer.domain.CustomerRegistrationRequest;
+import com.shahintraining.customer.repo.CustomerRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-public record CustomerService(){
+public record CustomerService(CustomerRepository customerRepository) {
 
     public void register(CustomerRegistrationRequest customerRegistrationRequest) {
         log.info("registering customer : {}", customerRegistrationRequest.firstName());
-        Customer customer = Customer.builder().firstName(customerRegistrationRequest.firstName())
+        Customer customer = new Customer().firstName(customerRegistrationRequest.firstName())
                 .lastName(customerRegistrationRequest.lastName())
-                .email(customerRegistrationRequest.email()).build();
+                .email(customerRegistrationRequest.email());
+        customerRepository.save(customer);
     }
 }
